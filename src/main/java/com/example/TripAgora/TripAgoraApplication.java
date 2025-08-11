b8eed3a2f6458b94065da59e8abd3817a3e187dd
JungYoon <canach3@gmail.com>
2025-08-11 20:23:33 +0900

    Initial commit

---
+++ src/main/java/com/example/TripAgora/TripAgoraApplication.java
@@ -0,0 +1,13 @@
+package com.example.TripAgora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripAgoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripAgoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TripAgora/TripAgoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TripAgora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripAgoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

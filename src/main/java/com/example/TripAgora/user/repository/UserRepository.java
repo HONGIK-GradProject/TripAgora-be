0dee03bfde5b3d05b18a13bffe4cde541a16163f
JungYoon <canach3@gmail.com>
2025-09-02 10:40:51 +0900

    [#29]feat: UserRepository 생성

--- src/main/java/com/example/TripAgora/user/repository/UserRepository.java
+++ src/main/java/com/example/TripAgora/user/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package com.example.TripAgora.user.repository;
 
+import com.example.TripAgora.user.entity.SocialType;
 import com.example.TripAgora.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findBySocialIdAndSocialType(String socialId, SocialType type);
 }

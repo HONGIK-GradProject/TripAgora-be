579e19e041a260c119351deb0026c2050184322b
JungYoon <canach3@gmail.com>
2025-09-10 17:19:00 +0900

    [#42]feat: GuideProfile 및 관련 엔티티 생성

---
+++ src/main/java/com/example/TripAgora/guideProfile/entity/GuidePortfolio.java
@@ -0,0 +1,43 @@
+package com.example.TripAgora.guideProfile.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "guide_portfolio")
+public class GuidePortfolio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "guide_profile_id", nullable = false)
+    private GuideProfile guideProfile;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type", nullable = false)
+    private PortfolioType type;
+
+    @Column(name = "url", nullable = false)
+    private String url;
+
+    @Builder
+    private GuidePortfolio(GuideProfile guideProfile, PortfolioType type, String url) {
+        this.guideProfile = guideProfile;
+        this.type = type;
+        this.url = url;
+    }
+
+    protected void updateUrl(String url) {
+        this.url = url;
+    }
+
+    public enum PortfolioType {
+        FACEBOOK, INSTAGRAM, TWITTER, WEBSITE, YOUTUBE
+    }
+}

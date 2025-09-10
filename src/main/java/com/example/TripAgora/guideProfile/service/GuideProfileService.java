e1bc0b6c2fc6a5310ab69d1d4838ba5ab66dd3e9
JungYoon <canach3@gmail.com>
2025-09-10 17:22:34 +0900

    [#42]feat: 역할 전환 로직 및 API 구현

---
+++ src/main/java/com/example/TripAgora/guideProfile/service/GuideProfileService.java
@@ -0,0 +1,55 @@
+package com.example.TripAgora.guideProfile.service;
+
+import com.example.TripAgora.guideProfile.entity.GuideProfile;
+import com.example.TripAgora.guideProfile.exception.AlreadyGuideException;
+import com.example.TripAgora.guideProfile.exception.AlreadyTravelerException;
+import com.example.TripAgora.guideProfile.repository.GuideProfileRepository;
+import com.example.TripAgora.user.dto.GuideSwitchResponse;
+import com.example.TripAgora.user.dto.TravelerSwitchResponse;
+import com.example.TripAgora.user.entity.User;
+import com.example.TripAgora.user.exception.UserNotFoundException;
+import com.example.TripAgora.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class GuideProfileService {
+    private final UserRepository userRepository;
+    private final GuideProfileRepository guideProfileRepository;
+
+    @Transactional
+    public GuideSwitchResponse switchToGuide(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+
+        if (user.getRole() == User.Role.GUIDE) {
+            throw new AlreadyGuideException();
+        }
+
+        GuideProfile profile = null;
+        if (user.getGuideProfile() == null) {
+            GuideProfile newProfile = GuideProfile.builder()
+                    .user(user)
+                    .build();
+            profile = guideProfileRepository.save(newProfile);
+        } else {
+            profile = user.getGuideProfile();
+        }
+
+        user.updateRole(User.Role.GUIDE);
+        return new GuideSwitchResponse(profile.getId()); // TODO: 가이드용 홈화면 확정 후 수정
+    }
+
+    @Transactional
+    public TravelerSwitchResponse switchToTraveler(Long userId) {
+        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+
+        if (user.getRole() == User.Role.TRAVELER) {
+            throw new AlreadyTravelerException();
+        }
+
+        user.updateRole(User.Role.TRAVELER);
+        return new TravelerSwitchResponse(user.getId()); // TODO: 여행객용 홈화면 확정 후 수정
+    }
+}

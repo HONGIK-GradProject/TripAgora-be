016af0d51a82fd0dd503129a24756eec1e5aeeb9
JungYoon <canach3@gmail.com>
2025-09-14 18:40:12 +0900

    [#44]feat: 템플릿 태그 변경 로직 및 API 작성

--- src/main/java/com/example/TripAgora/tag/repository/TemplateTagRepository.java
+++ src/main/java/com/example/TripAgora/tag/repository/TemplateTagRepository.java
@@ -2,9 +2,11 @@ package com.example.TripAgora.tag.repository;
 
 import com.example.TripAgora.tag.entity.TemplateTag;
 import com.example.TripAgora.tag.entity.TemplateTagId;
+import com.example.TripAgora.template.entity.Template;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TemplateTagRepository extends JpaRepository<TemplateTag, TemplateTagId> {
+    void deleteByTemplate(Template template);
 }

eda9b9c28602b67267a70c2262d82b3c54e4e64e
JungYoon <canach3@gmail.com>
2025-09-14 18:38:38 +0900

    [#44]feat: 템플릿 태그 변경 관련 ErrorCode, 예외 추가

--- src/main/java/com/example/TripAgora/common/code/TagErrorCode.java
+++ src/main/java/com/example/TripAgora/common/code/TagErrorCode.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum TagErrorCode implements ResponseCode {
     MINIMUM_TAGS_REQUIRED(HttpStatus.BAD_REQUEST, "태그는 최소 3개 이상 선택해야 합니다."),
+    MAXIMUM_TAGS_EXCEED(HttpStatus.BAD_REQUEST, "태그는 최대 5개까지 선택 가능합니다."),
     INVALID_TAG_SELECTION(HttpStatus.BAD_REQUEST, "존재하지 않거나 중복된 태그 ID가 포함되어 있습니다.");
 
     private final HttpStatus httpStatus;

9a5160e829082f20f3b31d8507558202149c51df
JungYoon <canach3@gmail.com>
2025-09-14 14:24:23 +0900

    [#48]refactor: 누락된 @Column(name) 추가

--- src/main/java/com/example/TripAgora/auth/entity/RefreshToken.java
+++ src/main/java/com/example/TripAgora/auth/entity/RefreshToken.java
@@ -20,7 +20,7 @@ public class RefreshToken {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @Column(nullable = false, unique = true)
+    @Column(name = "token", nullable = false, unique = true)
     private String token;
 
     @Builder

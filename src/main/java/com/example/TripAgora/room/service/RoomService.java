7c6a702f3fbba86ab24ae69917610882f24b1661
JungYoon <canach3@gmail.com>
2025-11-06 17:14:04 +0900

    [#100]fix: findByUserAndSession -> findByUser_IdAndSession_Id 누락된 부분 수정

--- src/main/java/com/example/TripAgora/room/service/RoomService.java
+++ src/main/java/com/example/TripAgora/room/service/RoomService.java
@@ -7,10 +7,7 @@ import com.example.TripAgora.participation.repository.ParticipationRepository;
 import com.example.TripAgora.room.entity.Room;
 import com.example.TripAgora.room.exception.RoomNotFoundException;
 import com.example.TripAgora.room.repository.RoomRepository;
-import com.example.TripAgora.session.entity.Session;
 import com.example.TripAgora.user.entity.Role;
-import com.example.TripAgora.user.entity.User;
-import com.example.TripAgora.user.exception.UserNotFoundException;
 import com.example.TripAgora.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -23,10 +20,9 @@ public class RoomService {
     private final ParticipationRepository participationRepository;
 
     protected Room checkRoomAndGuide(long userId, long roomId) {
-        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
         Room room = roomRepository.findById(roomId).orElseThrow(RoomNotFoundException::new);
-        Session session = room.getSession();
-        Participation participation = participationRepository.findByUserAndSession(user, session).orElseThrow(ParticipationNotFoundException::new);
+        Long sessionId = room.getSession().getId();
+        Participation participation = participationRepository.findByUser_IdAndSession_Id(userId, sessionId).orElseThrow(ParticipationNotFoundException::new);
 
         if (participation.getRole() != Role.GUIDE) {
             throw new AccessDeniedException();
@@ -36,10 +32,9 @@ public class RoomService {
     }
 
     protected Room checkRoomAndParticipant(long userId, long roomId) {
-        User user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
         Room room = roomRepository.findById(roomId).orElseThrow(RoomNotFoundException::new);
-        Session session = room.getSession();
-        participationRepository.findByUserAndSession(user, session).orElseThrow(ParticipationNotFoundException::new);
+        Long sessionId = room.getSession().getId();
+        participationRepository.findByUser_IdAndSession_Id(userId, sessionId).orElseThrow(ParticipationNotFoundException::new);
 
         return room;
     }

b1ee162efd90f34fe905713543e7249e9d4f1b23
JungYoon <canach3@gmail.com>
2025-11-06 18:27:40 +0900

    [#100]feat: 스케쥴링 적용(세션 상태 RECRUITMENT_CLOSED -> IN_PROGRESS, IN_PROGRESS -> COMPLETED)

--- src/main/java/com/example/TripAgora/TripAgoraApplication.java
+++ src/main/java/com/example/TripAgora/TripAgoraApplication.java
@@ -2,7 +2,9 @@ package com.example.TripAgora;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class TripAgoraApplication {
     public static void main(String[] args) {

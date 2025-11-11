ce61b1884e000f70aa22cdc1f1374d087ff3992b
JungYoon <canach3@gmail.com>
2025-11-11 12:08:06 +0900

    [#111]feat: ChatMessage 엔티티 생성

---
+++ src/main/generated/com/example/TripAgora/chat/entity/QChatMessage.java
@@ -0,0 +1,64 @@
+package com.example.TripAgora.chat.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QChatMessage is a Querydsl query type for ChatMessage
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QChatMessage extends EntityPathBase<ChatMessage> {
+
+    private static final long serialVersionUID = 1932455246L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QChatMessage chatMessage = new QChatMessage("chatMessage");
+
+    public final com.example.TripAgora.common.entity.QBaseEntity _super = new com.example.TripAgora.common.entity.QBaseEntity(this);
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    public final com.example.TripAgora.room.entity.QRoom room;
+
+    public final com.example.TripAgora.user.entity.QUser sender;
+
+    public QChatMessage(String variable) {
+        this(ChatMessage.class, forVariable(variable), INITS);
+    }
+
+    public QChatMessage(Path<? extends ChatMessage> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QChatMessage(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QChatMessage(PathMetadata metadata, PathInits inits) {
+        this(ChatMessage.class, metadata, inits);
+    }
+
+    public QChatMessage(Class<? extends ChatMessage> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.room = inits.isInitialized("room") ? new com.example.TripAgora.room.entity.QRoom(forProperty("room"), inits.get("room")) : null;
+        this.sender = inits.isInitialized("sender") ? new com.example.TripAgora.user.entity.QUser(forProperty("sender"), inits.get("sender")) : null;
+    }
+
+}
+

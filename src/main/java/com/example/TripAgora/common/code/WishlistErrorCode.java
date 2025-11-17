2469770726b06636bd5c0cf09fa1729f01729376
JungYoon <canach3@gmail.com>
2025-11-17 21:31:15 +0900

    [#112]refactor: 에러메시지 수정(세션 -> 여행, 템플릿 -> 플랜)

--- src/main/java/com/example/TripAgora/common/code/WishlistErrorCode.java
+++ src/main/java/com/example/TripAgora/common/code/WishlistErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum WishlistErrorCode implements ResponseCode {
-    ALREADY_IN_WISHLIST(HttpStatus.CONFLICT, "이미 위시리스트에 추가한 세션입니다."),
+    ALREADY_IN_WISHLIST(HttpStatus.CONFLICT, "이미 위시리스트에 추가한 여행입니다."),
     WISHLIST_NOT_FOUND(HttpStatus.NOT_FOUND, "위시리스트 내역을 찾을 수 없습니다.");
 
     private final HttpStatus httpStatus;
